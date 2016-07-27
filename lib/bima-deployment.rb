6f70283f5116e90b5a55c84d9a91acecfdd4fdcb
Joergen Dahlke <joergen.dahlke@infopark.de>
2016-07-27 12:30:45 +0200

    add basic configuration and rails integration

--- lib/bima-deployment.rb
+++ lib/bima-deployment.rb
@@ -1,4 +1,46 @@
 require 'aws-sdk'
+require 'logger'
 require 'rake'
 
 require 'bima_deployment/version'
+require 'bima_deployment/deployment'
+require 'bima_deployment/rails'
+
+module BimaDeployment
+  mattr_accessor :logger, :s3
+  mattr_accessor :excluded_files, :excluded_dirs
+
+  def self.configure
+    yield self
+  end
+
+  #
+  # Initial configuration
+  #
+  logger = ::Logger.new(STDOUT)
+  logger.formatter = proc do |severity, datetime, progname, msg|
+    "#{msg}\n"
+  end
+  self.logger = logger
+
+  self.s3 = {
+    bucket_name: 'bima-releases-ireland',
+    region: 'eu-west-1'
+  }
+
+  self.excluded_files = %w(
+    config/settings.yml
+    config/aws.yml
+    config/crm.yml
+    config/database.yml
+    .gitignore
+    .travis.yml
+    .rspec
+  )
+  self.excluded_dirs = %w(
+    .git
+    client/node_modules
+    spec
+    test
+  )
+end
---
+++ lib/bima_deployment/rails.rb
@@ -0,0 +1,13 @@
+module BimaDeployment
+  if Object.const_defined?(:Rails) and Rails.const_defined?(:Railtie)
+    # @private
+    class Railtie < Rails::Railtie
+      rake_tasks do
+        load 'tasks/package.rake'
+      end
+
+      initializer 'bima_deployment.initialize' do |app|
+      end
+    end
+  end
+end
