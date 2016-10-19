3cfe4128db3794d80a0df68a20aa6c1b2d8f6723
Huy Dinh <mail@huydinh.eu>
2016-10-19 11:34:53 +0200

    Move BimaDeployment::Notifications::Slack#template to `protected`.

--- lib/bima_deployment/notifications/slack.rb
+++ lib/bima_deployment/notifications/slack.rb
@@ -9,15 +9,15 @@ module BimaDeployment
         @template = options[:template]
       end
 
-      def template
-        @template || "%s just deployed `%s` on `%s`.\n\n%s"
-      end
-
       def notify(deployer, name, ref, url)
         Net::HTTP.post_form(@webhook_uri, payload: payload(deployer, name, ref, url))
       end
 
       protected
+
+      def template
+        @template || "%s just deployed `%s` on `%s`.\n\n%s"
+      end
 
       def payload(deployer, name, ref, url)
         {
