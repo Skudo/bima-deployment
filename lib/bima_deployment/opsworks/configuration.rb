57bd6539e101f65e18ff0f93b0eae44c7cbfaab6
Huy Dinh <mail@huydinh.eu>
2016-10-24 10:31:06 +0200

    Only load Opsworks credentials, if a credentials file exists.

--- lib/bima_deployment/opsworks/configuration.rb
+++ lib/bima_deployment/opsworks/configuration.rb
@@ -2,6 +2,9 @@ module BimaDeployment
   module Opsworks
     class Configuration
       def self.load_credentials(profile = nil)
+        return unless File.exists?(Aws.shared_config.config_path) ||
+                      File.exists?(Aws.shared_config.credentials_path)
+
         begin
           profile ||= 'bima'
           shared_credentials = Aws::SharedCredentials.new(profile_name: profile)
