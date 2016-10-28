50953fc657de5167e1936e18c9cd2be6b1daed16
Huy Dinh <mail@huydinh.eu>
2016-10-28 10:58:40 +0200

    Add command line options `--strategy` and `--slack`.

--- lib/bima_deployment/rails.rb
+++ lib/bima_deployment/rails.rb
@@ -3,15 +3,25 @@ module BimaDeployment
     class InstallGenerator < Rails::Generators::NamedBase
       source_root File.expand_path('../../templates', __FILE__)
 
+      class_option :strategy, type: :string, default: 'opsworks/s3'
+      class_option :slack, type: :string, default: 'https://hooks.slack.com/services/get-your-own-url'
+
       def copy_files
         config_files = %w(
           config/deployment.yml
           config/initializers/deployment.rb
         )
+        replacements = {
+          STACK: name,
+          APP: name.downcase,
+          STRATEGY: options[:strategy],
+          SLACK_URL: options[:slack]
+        }
         config_files.each do |config_file|
           copy_file(config_file)
-          gsub_file(config_file, 'STACK', name)
-          gsub_file(config_file, 'APP', name.downcase)
+          replacements.each_pair do |key, value|
+            gsub_file(config_file, key.to_s, value.to_s)
+          end
         end
       end
     end
